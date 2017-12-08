1e02d6ca96cec840a3011ec30838a82e2399ef20
vscing <513335394@qq.com.com>
2017-12-08 14:22:25 +0800

    提交文件

---
+++ src/funciton.php
@@ -0,0 +1,36 @@
+<?php
+namespace VscWork;
+
+class funciton
+{
+    //用户昵称
+    public  $user = '';
+    //用户操作
+    private $operation = '';
+    //操作码
+    private $password;
+
+    function  __construct($user,$operation,$password)
+    {
+        $this->user = $user;
+        $this->operation = $operation;
+        $this->password = $password;
+        $this->check();
+    }
+
+    //验证用户码
+    public function check()
+    {
+        if(!$this->password === 666666)
+        {
+            return '操作码错误';
+        }else {
+            $this->hello($this->operation);
+        }
+    }
+    //输出
+    public function hello($operation)
+    {
+        echo $operation;
+    }
+}
